da3be5fc6a189882b5506c94661bd8526653a9ba
Daniel <info@daniel-bird.co.uk>
2022-07-08 17:49:40 +0100

    Fix imports

--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,5 +1,6 @@
 import type { Joke } from '@prisma/client';
-import { json, LoaderFunction } from '@remix-run/node';
+import type { LoaderFunction } from '@remix-run/node';
+import { json } from '@remix-run/node';
 import { Link, useLoaderData } from '@remix-run/react';
 import { db } from '~/utils/db.server';
 
