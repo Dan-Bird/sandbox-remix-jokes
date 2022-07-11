3a199a9a4e617aa13cf6b17bfc7cd3fcf0b8173b
Daniel <info@daniel-bird.co.uk>
2022-07-11 13:55:44 +0100

    Grab user when trying to login

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -2,6 +2,7 @@ import type { ActionFunction, LinksFunction } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import { Link, useActionData, useSearchParams } from '@remix-run/react';
 import { db } from '~/utils/db.server';
+import { login } from '~/utils/session.server';
 import stylesUrl from '../styles/login.css';
 
 export const links: LinksFunction = () => {
@@ -71,8 +72,14 @@ export const action: ActionFunction = async ({ request }) => {
 
   switch (loginType) {
     case 'login': {
-      // login to get the user
-      // if there's no user, return the fields and a formError
+      const user = await login({ username, password });
+      console.log({ user });
+      if (!user) {
+        return badRequest({
+          formError: 'Username/Password combination is incorrect',
+          fields,
+        });
+      }
       // if there is a user, create their session and redirect to /jokes
       return badRequest({
         fields,
---
+++ app/utils/session.server.ts
@@ -0,0 +1,23 @@
+import { db } from './db.server';
+import bcrypt from 'bcryptjs';
+
+type LoginForm = {
+  username: string;
+  password: string;
+};
+
+export async function login({ username, password }: LoginForm) {
+  const user = await db.user.findUnique({
+    where: { username },
+  });
+  if (!user) {
+    return null;
+  }
+
+  const isCorrectPassword = await bcrypt.compare(password, user.passwordHash);
+  if (!isCorrectPassword) {
+    return null;
+  }
+
+  return { id: user.id, username };
+}
