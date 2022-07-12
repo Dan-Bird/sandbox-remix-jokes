12ba3ab3b71889acec14799f51db4dc9e6e18c9e
Daniel <info@daniel-bird.co.uk>
2022-07-12 10:21:16 +0100

    Add CatchBoundary for expected errors

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,7 +1,7 @@
 import type { Joke } from '@prisma/client';
 import type { LoaderFunction } from '@remix-run/node';
 import { json } from '@remix-run/node';
-import { Link, useLoaderData, useParams } from '@remix-run/react';
+import { Link, useCatch, useLoaderData, useParams } from '@remix-run/react';
 import { db } from '~/utils/db.server';
 
 type LoaderData = { joke: Joke };
@@ -12,7 +12,7 @@ export const loader: LoaderFunction = async ({ params }) => {
   });
 
   if (!joke) {
-    throw new Error('Joke not found');
+    throw new Response('What a joke! Not found', { status: 404 });
   }
 
   const data: LoaderData = { joke };
@@ -29,6 +29,20 @@ export default function JokeRoute() {
       <Link to=".">{data.joke.name}</Link>
     </div>
   );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+  const params = useParams();
+
+  if (caught.status === 404) {
+    return (
+      <div className="error-container">
+        Huh? What the heck is "{params.jokeId}"?
+      </div>
+    );
+  }
+  throw new Error(`Unhandled error: ${caught.status}`);
 }
 
 export function ErrorBoundary() {
--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,10 +1,18 @@
-import type { ActionFunction } from '@remix-run/node';
+import type { ActionFunction, LoaderFunction } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import { redirect } from '@remix-run/node';
-import { useActionData } from '@remix-run/react';
+import { Link, useActionData, useCatch } from '@remix-run/react';
 
 import { db } from '~/utils/db.server';
-import { requireUserId } from '~/utils/session.server';
+import { getUserId, requireUserId } from '~/utils/session.server';
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const userId = await getUserId(request);
+  if (!userId) {
+    throw new Response('Unauthorised', { status: 401 });
+  }
+  return json({});
+};
 
 function validateJokeContent(content: string) {
   if (content.length < 10) {
@@ -123,6 +131,19 @@ export default function NewJokeRoute() {
       </form>
     </div>
   );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  if (caught.status === 401) {
+    return (
+      <div className="error-container">
+        <p>You must be logged in to create a joke</p>
+        <Link to="/login">Login</Link>
+      </div>
+    );
+  }
 }
 
 export function ErrorBoundary() {
